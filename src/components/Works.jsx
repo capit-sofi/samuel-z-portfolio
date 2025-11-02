1e60078a4482c23e9e9b4c11342cd6d60576053d
sofoniyas alemu <sofiprogram1996@gmaile.com>
2025-11-02 11:40:21 +0300

    3-commit

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -83,9 +83,9 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className='mt-20 flex flex-wrap justify-center gap-7'>
         {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
+          <ProjectCard key={`project-${index}`} index={index} {...project} className='relative w-full h-auto sm:h-[230px]' />
         ))}
       </div>
     </>
